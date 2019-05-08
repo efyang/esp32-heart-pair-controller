271b6d89b6d27ff8d9337b0cddebf60aed514fd5
Edward Yang <edward.yang6771@gmail.com>
2019-05-08 15:44:21 -0500

    Add color displays

--- app/src/main/java/com/example/esp32heartpaircontroller/MainActivity.kt
+++ app/src/main/java/com/example/esp32heartpaircontroller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.esp32heartpaircontroller
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.Manifest.permission.READ_CONTACTS
 import android.os.Bundle
 import android.provider.ContactsContract
@@ -17,10 +18,19 @@ import android.database.Cursor
 import android.view.Menu
 import android.view.MenuItem
 import android.R.id.edit
+import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Build
+import android.provider.Settings
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.clj.fastble.BleManager
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
@@ -56,8 +66,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setBluetooth(true);
+        checkPermissions()
+        BleManager.getInstance().init(getApplication());
+        BleManager.getInstance()
+                .enableLog(true)
+                .setReConnectCount(1, 5000)
+                .setConnectOverTime(20000)
+                .setOperateTimeout(5000);
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         loadFragment(HomeFragment())
+    }
+
+    fun setBluetooth(enable: Boolean): Boolean {
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        val isEnabled = bluetoothAdapter.isEnabled
+        if (enable && !isEnabled) {
+            return bluetoothAdapter.enable()
+        } else if (!enable && isEnabled) {
+            return bluetoothAdapter.disable()
+        }
+        // No need to change bluetooth state
+        return true
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -74,5 +105,78 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE_PERMISSION_LOCATION -> if (grantResults.size > 0) {
+                for (i in grantResults.indices) {
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        onPermissionGranted(permissions[i])
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkPermissions() {
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (!bluetoothAdapter.isEnabled) {
+            Toast.makeText(this, getString(R.string.please_open_blue), Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        val permissionDeniedList = ArrayList<String>()
+        for (permission in permissions) {
+            val permissionCheck = ContextCompat.checkSelfPermission(this, permission)
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                onPermissionGranted(permission)
+            } else {
+                permissionDeniedList.add(permission)
+            }
+        }
+        if (!permissionDeniedList.isEmpty()) {
+            val deniedPermissions = permissionDeniedList.toTypedArray<String>()
+            ActivityCompat.requestPermissions(this, deniedPermissions, REQUEST_CODE_PERMISSION_LOCATION)
+        }
+    }
+
+    private fun onPermissionGranted(permission: String) {
+        when (permission) {
+            android.Manifest.permission.ACCESS_FINE_LOCATION -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !checkGPSIsOpen()) {
+                AlertDialog.Builder(this)
+                        .setTitle(R.string.notifyTitle)
+                        .setMessage(R.string.gpsNotifyMsg)
+                        .setNegativeButton(R.string.cancel
+                        ) { dialog, which -> finish() }
+                        .setPositiveButton(R.string.setting
+                        ) { dialog, which ->
+                            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                            startActivityForResult(intent, REQUEST_CODE_OPEN_GPS)
+                        }
+
+                        .setCancelable(false)
+                        .show()
+            } else {
+            }
+        }
+    }
+
+    private fun checkGPSIsOpen(): Boolean {
+        val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                ?: return false
+        return locationManager.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER)
+    }
+
+    companion object {
+
+        private val TAG = MainActivity::class.java!!.getSimpleName()
+        private val REQUEST_CODE_OPEN_GPS = 1
+        private val REQUEST_CODE_PERMISSION_LOCATION = 2
     }
 }
